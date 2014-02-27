d79623150f0012512510582508f9993665ef3de7
Mindy Preston <meetup@yomimono.org>
2014-02-27 15:17:35 -0500

    Programming is hard

--- rfk.elm
+++ rfk.elm
@@ -13,11 +13,10 @@ import List
 --edge detection (done?)
 
 type Item a = { a | char:String, description:String, xd:Int, yd:Int }
-type Colliding a = { a | collidingWith: Item {} }
-robot = { char = "@", xd = 0, yd = 0, description = "Robot, sans kitten." }
+type Colliding b = { b | collidingWith: String }
+robot = { char = "@", xd = 0, yd = 0, description = "Robot, sans kitten.", collidingWith = "" }
 characters = "$%^&*()qwertyuiop[]{}asdfghjkl;:zxcvbnm,.<>"
-item = { char = "#", description = "An item.", xd = 2, yd = 2}
-items = [ item ]
+items = [ { char = "#", description = "An item.", xd = 2, yd = 2} ]
 
 nextPoint : (Int, Int) -> (Int, Int) -> Element -> (Float, Float)
 nextPoint (x, y) (w', h') roboElem =
@@ -38,7 +37,7 @@ samePlace robot item = robot.xd == item.xd && robot.yd == item.yd
 --Determine the case where the robot is investigating an object.
 --The robot shouldn't be able to collide with multiple things simultaneously,
 --but if this does occur, we'll just take the first object.
-collision : Item a -> [Item a] -> Maybe (Item a)
+collision : Item b -> [Item a] -> Maybe (Item a)
 collision robot items = 
   let found = (List.filter (samePlace robot) items )
   in if List.isEmpty found then Nothing else Just (head found)
@@ -48,24 +47,30 @@ getDescription x = case x of
   Just x -> x.description
   Nothing -> "NOT KITTEN"
 
-render : (Int, Int) -> Item a-> Element
+--convenience function for looking up what message ought to be displayed
+--based on whether our robot's just investigated something and hasn't yet 
+--moved away.
+getMessage : Colliding a -> Element
+getMessage r = Text.text (fontify robot.collidingWith)
+
+render : (Int, Int) -> Colliding (Item {}) -> Element
 render (w, h) robot =
   let roboElem = Text.text ( fontify robot.char )
-      message = Text.text ( fontify (getDescription (collision robot items)))
   in collage w h [
-    filled black (rect (toFloat w) (toFloat h))
-  , move (nextPoint (robot.xd, robot.yd) (w, h) roboElem) (toForm roboElem)
-  , move (nextPoint (robot.xd, robot.yd - 1) (w, h) roboElem) (toForm message) 
-  , move (nextPoint (item.xd, item.yd) (w, h) roboElem) (toForm (Text.text (fontify item.char)))
-  ] --and add all of the items to this list as well
+      filled black (rect (toFloat w) (toFloat h))
+      , move (nextPoint (robot.xd, robot.yd) (w, h) roboElem) (toForm roboElem)
+      , move (nextPoint (robot.xd, robot.yd - 1) (w, h) roboElem) (toForm (getMessage robot)) 
+      , move (nextPoint ((head items).xd, (head items).yd) (w, h) roboElem) (toForm (Text.text (fontify (head items).char)))
+    ] --and add all of the items to this list as well
 
+updatePosition : Item a -> (Int, Int) -> Item a
+updatePosition r (x, y) = {r | xd <- r.xd + x, yd <- r.yd + y}
 
-step : {x:Int, y:Int} -> Item a-> Item a
-step {x, y} ({char, xd, yd} as r) = 
-    --disallow movement over another item
-    let next_robot = { char = r.char, xd = r.xd + x, yd = r.yd + y, description = r.description}
-        investigating = collision next_robot items
-    in if isJust investigating then { r | collidingWith = investigating } else next_robot
+step : {x:Int, y:Int} -> Colliding(Item {}) -> Colliding(Item {})
+step {x, y} ({xd, yd, collidingWith} as r) = 
+  case (collision (updatePosition r (x, y)) items) of
+    Just otherItem -> r --{ r | collidingWith = otherItem.description }
+    Nothing -> updatePosition r (x, y)
 
 input : Signal {x:Int, y:Int}
 input = --let delta = lift (\t -> t/20) (fps 25)

50ebeef70908d4e9ad253dafea9ae93957f1f7ea
Mindy Preston <meetup@yomimono.org>
2014-03-03 16:51:05 -0500

    Refactored "pick a random element from the list" is in here.

--- SimpleRandom.elm
+++ SimpleRandom.elm
@@ -23,8 +23,19 @@ makeSomeRandomItems howMany seed =
         y = Random.range 0 200 seed
     in (lift2 makeItem x y) :: (makeSomeRandomItems (howMany - 1) seed)
 
+index : Int -> [a] -> a
+index number list = 
+  last (take number list)
+
+randomListItem : Signal Int -> [Signal a] -> Signal a
+randomListItem gen list =
+  if length list == 1 then (head list)
+  else 
+    let randomIndex = (Random.range 1 (length list) gen)
+    in lift2 index randomIndex (combine list)
+
 main =
   let ofInterest = makeSomeRandomItems 10 initialSeed
-  in lift asText (combine ofInterest)
+  in lift asText (randomListItem initialSeed ofInterest)
   --lift asText (constant [1, 2, 3, 4])
   --in lift asText (lift2 (==) (head ofInterest) (last ofInterest))
